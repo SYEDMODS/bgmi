308318c4149047e6124ed891794457218da26e84
SYEDMODS <154255018+SYEDMODS@users.noreply.github.com>
2024-06-19 01:33:26 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -252,7 +252,7 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
+            if time > 500:
                 response = "Error: Time interval must be less than 80."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
